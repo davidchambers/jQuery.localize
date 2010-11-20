399edc855ffd9a591084d117dfc1f5e6f62e86ae
David Chambers <David.Chambers.05@gmail.com>
2010-11-20 19:59:46 +1100

    initial commit

---
+++ jquery.localize.js
@@ -0,0 +1,168 @@
+/*
+ * jQuery plugin for localizing dates and times via
+ * the datetime attribute of the HTML5 <time> element.
+ *
+ * https://github.com/davidchambers/jQuery.localize
+ */
+
+(function ($) {
+
+    var settings = {
+        abbrDays: 'Sun Mon Tues Wed Thurs Fri Sat'.split(' '),
+        abbrMonths: 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' '),
+        format: 'd mmmm yyyy',
+        fullDays: 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday'.split(' '),
+        fullMonths: 'January February March April May June July August September October November December'.split(' '),
+        ordinals: function (n) {
+            if (10 < n && n < 14) return 'th';
+            switch (n % 10) {
+                case 1: return 'st';
+                case 2: return 'nd';
+                case 3: return 'rd';
+                default: return 'th';
+            }
+        },
+        periods: ['AM', 'PM']
+    };
+
+    function load(options) {
+        jQuery.extend(settings, options);
+        return this;
+    }
+
+    function localize(options) {
+
+        options = jQuery.extend({}, settings, typeof options == 'string' ? { format: options } : options);
+
+        var f = {
+
+            yy: function (date) {
+                var s = pad(date.getFullYear());
+                return s.substr(s.length - 2);
+            },
+
+            yyyy: function (date) {
+                return date.getFullYear();
+            },
+
+            m: function (date) {
+                return date.getMonth() + 1;
+            },
+
+            mm: function (date) {
+                return pad(date.getMonth() + 1);
+            },
+
+            mmm: function (date) {
+                return options.abbrMonths[date.getMonth()];
+            },
+
+            mmmm: function (date) {
+                return options.fullMonths[date.getMonth()];
+            },
+
+            d: function (date) {
+                return date.getDate();
+            },
+
+            dd: function (date) {
+                return pad(date.getDate());
+            },
+
+            ddd: function (date) {
+                return options.abbrDays[date.getDay()];
+            },
+
+            dddd: function (date) {
+                return options.fullDays[date.getDay()];
+            },
+
+            S: function (date) {
+                return options.ordinals(date.getDate());
+            },
+
+            h: function (date) {
+                return date.getHours() % 12 || 12;
+            },
+
+            hh: function (date) {
+                return pad(date.getHours() % 12 || 12);
+            },
+
+            H: function (date) {
+                return date.getHours();
+            },
+
+            HH: function (date) {
+                return pad(date.getHours());
+            },
+
+            M: function (date) {
+                return date.getMinutes();
+            },
+
+            MM: function (date) {
+                return pad(date.getMinutes());
+            },
+
+            s: function (date) {
+                return date.getSeconds();
+            },
+
+            ss: function (date) {
+                return pad(date.getSeconds());
+            },
+
+            a: function (date) {
+                return options.periods[date.getHours() < 12 ? 0 : 1];
+            },
+
+            Z: function (date) {
+                var mins = -date.getTimezoneOffset(), sign = mins > 0 ? '+' : '-';
+                mins >= 0 || (mins = -mins);
+                return sign + pad(mins/60 >> 0) + ':' + pad(mins % 60);
+            }
+        },
+
+        re = /^(\d{4})-(\d\d)-(\d\d)T(\d\d):(\d\d)(?::(\d\d))?(?:[-+]00:00|Z)$/;
+
+        return this.each(function () {
+            var $this = $(this), date = $this.attr('datetime'), m;
+            if (date && (m = date.match(re))) {
+                date = new Date(Date.UTC(m[1]*1, m[2]*1 - 1, m[3]*1, m[4]*1, m[5]*1, m[6]*1 || 0));
+                $this.attr('datetime', formatDate(date, 'yyyy-mm-ddTHH:MM' + (m[6] ? ':ss' : '') + 'Z'));
+                $this.text(formatDate(date, options.format));
+            }
+        });
+
+        function formatDate(date, format) {
+            var count = 0, output = '', prev;
+            jQuery.each((format + '☺').split(''), function (index, char) {
+                ++count;
+                if (char !== prev) {
+                    if (prev) {
+                        var fragment = (new Array(count)).join(prev);
+                        output += f.hasOwnProperty(fragment) ? f[fragment](date) : fragment;
+                    }
+                    count = 1;
+                    prev = char;
+                }
+            });
+            return output;
+        }
+
+        function pad(s, n) {
+            s += '';
+            n || (n = 2);
+            var delta = n - s.length;
+            return delta > 0 ? (new Array(++delta)).join('0') + s : s;
+        }
+    }
+
+    jQuery.fn.localize = function (method) {
+        return (method == 'load' ?
+                load.apply(this, Array.prototype.slice.call(arguments, 1)) :
+                localize.apply(this, arguments));
+    };
+
+}(jQuery));
---
+++ tests.js
@@ -0,0 +1,110 @@
+(function ($) {
+
+    var fails = 0, passes = 0,
+        d1 = new Date(Date.UTC(2008, 6-1, 4, 2)),
+        d2 = new Date(Date.UTC(2010, 11-1, 12, 13, 14, 15)),
+        t1 = '<time datetime="2008-06-04T02:00-00:00"></time>',
+        t2 = '<time datetime="2010-11-12T13:14:15Z"></time>',
+        assert = {
+            equal: function (a, b) {
+                a === b ? ++passes : ++fails;
+            },
+            match: function (text, re) {
+                this.equal(text.match(re) ? true : false, true);
+            }
+        };
+
+    // Rudimentary directive tests
+
+    assert.equal($(t1).localize('yy').text(), '08');
+    assert.equal($(t1).localize('yyyy').text(), '2008');
+
+    assert.equal($(t1).localize('m').text(), '6');
+    assert.equal($(t1).localize('mm').text(), '06');
+    assert.equal($(t1).localize('mmm').text(), 'Jun');
+    assert.equal($(t1).localize('mmmm').text(), 'June');
+
+    assert.equal($(t2).localize('m').text(), '11');
+    assert.equal($(t2).localize('mm').text(), '11');
+
+    assert.equal($(t1).localize('d').text(), d1.getDate() + '');
+    assert.equal($(t2).localize('d').text(), d2.getDate() + '');
+
+    assert.equal($(t1).localize('dd').text().length, 2);
+    assert.equal($(t2).localize('dd').text().length, 2);
+
+    assert.match($(t1).localize('ddd').text(), /^(Mon|Tues|Wed|Thurs|Fri|Sat|Sun)$/);
+    assert.match($(t2).localize('ddd').text(), /^(Mon|Tues|Wed|Thurs|Fri|Sat|Sun)$/);
+
+    assert.match($(t1).localize('dddd').text(), /^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)$/);
+    assert.match($(t2).localize('dddd').text(), /^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)$/);
+
+    assert.match($(t1).localize('dS').text(), /^([23]?1st|2?2nd|2?3rd|(2?[4-9]|1\d|[23]0)th)$/);
+    assert.match($(t2).localize('dS').text(), /^([23]?1st|2?2nd|2?3rd|(2?[4-9]|1\d|[23]0)th)$/);
+
+    assert.equal($(t1).localize('h').text(), (d1.getHours() % 12 || 12) + '');
+    assert.equal($(t2).localize('h').text(), (d2.getHours() % 12 || 12) + '');
+
+    assert.equal($(t1).localize('hh').text().length, 2);
+    assert.equal($(t2).localize('hh').text().length, 2);
+
+    assert.equal($(t1).localize('H').text(), d1.getHours() + '');
+    assert.equal($(t2).localize('H').text(), d2.getHours() + '');
+
+    assert.equal($(t1).localize('HH').text().length, 2);
+    assert.equal($(t2).localize('HH').text().length, 2);
+
+    assert.equal($(t1).localize('M').text(), d1.getMinutes() + '');
+    assert.equal($(t2).localize('M').text(), d2.getMinutes() + '');
+
+    assert.equal($(t1).localize('MM').text().length, 2);
+    assert.equal($(t2).localize('MM').text().length, 2);
+
+    assert.equal($(t1).localize('s').text(), d1.getSeconds() + '');
+    assert.equal($(t2).localize('s').text(), d2.getSeconds() + '');
+
+    assert.equal($(t1).localize('ss').text().length, 2);
+    assert.equal($(t2).localize('ss').text().length, 2);
+
+    assert.equal($(t1).localize('a').text(), d1.getHours() < 12 ? 'AM' : 'PM');
+    assert.equal($(t2).localize('a').text(), d2.getHours() < 12 ? 'AM' : 'PM');
+
+    assert.match($(t1).localize('Z').text(), /[-+]\d\d:\d\d$/);
+    assert.match($(t2).localize('Z').text(), /[-+]\d\d:\d\d$/);
+
+    // Confirm graceful failure
+
+    try {
+        assert.equal($('<time datetime="2010-11-20">Initial release</time>').localize().text(), 'Initial release');
+    } catch (e) {
+        ++fails;
+    }
+
+    try {
+        assert.equal($('<b>jQuery</b>').localize().text(), 'jQuery');
+    } catch (e) {
+        ++fails;
+    }
+
+    // API tests
+
+    assert.match($(t1).localize().text(), /^[34] June 2008$/);
+    assert.match($(t2).localize().text(), /^1[23] November 2010$/);
+
+    assert.match($(t1).localize({ format: 'yyyy' }).text(), /^\d{4}$/);
+    assert.match($(t1).localize().text(), /^[34] June 2008$/);
+
+    $().localize('load', {
+        format: 'd mmmm yyyy',
+        fullDays: 'dimanche lundi mardi mercredi jeudi vendredi samedi'.split(' '),
+        fullMonths: 'janvier février mars avril mai juin juillet août septembre octobre novembre décembre'.split(' ')
+    });
+
+    assert.match($(t1).localize().text(), / juin /);
+    assert.match($(t1).localize('dddd').text(), /^(mardi|mercredi)$/);
+    assert.equal($(t1).localize({ format: 'yyyy' }).text(), '2008');
+    assert.match($(t1).localize().text(), / juin /);
+
+    window.alert([passes, 'of', passes + fails, 'tests succeeded'].join(' '));
+
+}(jQuery));
